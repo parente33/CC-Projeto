7640f72fa3497e27110b3903de8cb385f6820000
Pedro <a107292@alunos.uminho.pt>
2025-11-21 16:49:33 +0000

    Header do Protocolo MissionLink

---
+++ src/protocols/MissionHeader.py
@@ -0,0 +1,52 @@
+import struct
+
+class MissionHeader:
+    size = 24  #tamanho do header A DEFINIR
+    # Definir typess como constantes
+    TYPE_ACK    = 0b00000001
+    TYPE_SYN    = 0b00000010
+    TYPE_SYNACK = 0b00000100
+    TYPE_REQ    = 0b00010000 #So do lado do servidor
+    TYPE_DATA   = 0b00100000
+
+    #Flag Retransmissao
+
+    RETR = 0b00000001  # RETRANSMITIDO
+    DEF  = 0b00000000  #DEFAULT
+
+    def __init__(self, connection_ID = 0, seq_number = 0, ack_number = 0, type = TYPE_DATA, length = 0,offset = 0,frag_ID = 0, retr = DEF, req_number = 0):
+        self.connection_ID  = connection_ID                   # 4 bytes
+        self.seq_number     = seq_number                      # 4 bytes
+        self.ack_number     = ack_number                      # 4 bytes
+        self.type           = type                            # 1 byte
+        self.length         = length + MissionHeader.size     # 2 bytes Original antes da fragmentaçao
+        self.frag_ID        = frag_ID                         # 4 bytes
+        self.offset         = offset                          # 2 bytes
+        self.retr           = retr                            # 1 byte
+        self.req_number     = req_number                      # 2 bytes
+
+    def pack(self):
+        """
+        Converte os campos do header em bytes prontos para enviar via socket.
+        """
+        return struct.pack('!IIIBHIHBH',
+                           self.connection_ID,
+                           self.seq_number,
+                           self.ack_number,
+                           self.type,
+                           self.length,
+                           self.frag_ID,
+                           self.offset,
+                           self.retr,
+                           self.req_number
+                           )
+
+    @staticmethod
+    def unpack(data):
+        """
+        Recebe bytes e reconstrói o header original.
+        """
+        connection_ID,seq_number,ack_number,flags,length,frag_ID,offset, retr, req_number = struct.unpack('!IIIBHIHBH', data)
+        return MissionHeader(connection_ID, seq_number, ack_number,  flags,length,frag_ID, offset, retr, req_number)
+
+
